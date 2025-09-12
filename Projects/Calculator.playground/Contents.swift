eb127d6db10f2b5d822989fa2fa8ea6557b606b0
Ezra Pease <pieezra@gmail.com>
2025-09-12 09:41:20 -0600

    Created and started Calculator Project

---
+++ Projects/Calculator.playground/Contents.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+
+
+
+struct Calculator {
+    var firstValue: Double?
+    var secondValue: Double?
+    
+    func inputValues(firstValue: Double?, secondValue: Double?) {
+        
+    }
+    
+    func add() {
+        
+    }
+    
+    func subtract() {
+        
+    }
+    
+    func multiply() {
+        
+    }
+    
+    func divide() {
+        
+    }
+    
+    func invertSign() {
+        
+    }
+    
+    func percentage() {
+        
+    }
+    
+    mutating func clearValues() {
+        firstValue = nil
+        secondValue = nil
+    }
+}
