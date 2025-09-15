7dc07263fa987b0c38b4ea2732f31220fa805e62
Ezra Pease <pieezra@gmail.com>
2025-09-15 11:53:19 -0600

    Progress on Calculator

--- Projects/Calculator.playground/Contents.swift
+++ Projects/Calculator.playground/Contents.swift
@@ -1,21 +1,26 @@
 import UIKit
 
+//Your overall goal is to recreate the functionality of an actual calculator as possible while using functions in the place of buttons. To that end, your mathematical functions must not accept any parameters. Instead, you must have one function for inputting a value, then "press" the other functions for adding, subtracting, multiplying, dividing, invert sign (+/-) and percentage, as well as C/AC to clear the current value. Use print statements to output the running total of the calculator.
+
+
+
+
 enum Operator {
     case add
     case subtract
     case multiply
     case divide
     case invertSign
-    case none
 }
 
 var currentValue: Double? = nil
-var result: Double?
 var isPressed: Bool = false
-var currentOperator: Operator = .none
+var currentOperator: Operator = .add
+var operatorCount = 0
 
 var value1: [String] = []
 var value2: [String] = []
+var combinedValues: [String] = []
 
 
 
@@ -36,51 +41,56 @@ var value2: [String] = []
 @MainActor func add() {
     currentOperator = .add
     isPressed = true
+    operatorCount += 1
 }
 
 @MainActor func subtract() {
     currentOperator = .subtract
     isPressed = true
-    
+    operatorCount += 1
+
 }
 
 @MainActor func multiply() {
     currentOperator = .multiply
     isPressed = true
-    
+    operatorCount += 1
+
 }
 
 @MainActor func divide() {
     currentOperator = .divide
     isPressed = true
-    
+    operatorCount += 1
+
 }
 
 @MainActor func invertSign() {
     currentOperator = .invertSign
-    isPressed = true
     
 }
 
 //@MainActor func percentage() {
 //    currentOperator = .percentage
 //    isPressed = true
-//    
+//
 //}
 
+// C/AC
 @MainActor func clearValues() {
     currentValue = nil
     isPressed = false
-    currentOperator = .none
     value1.removeAll()
     value2.removeAll()
+    combinedValues.removeAll()
+    operatorCount = 0
 }
 
 
 
 @MainActor func equals() {
-    if let doubleValue1 = Double(value1.joined()),
-       let doubleValue2 = Double(value2.joined()) {
+    if var doubleValue1 = Double(value1.joined()),
+       var doubleValue2 = Double(value2.joined()) {
         switch currentOperator {
         case .add:
             result = doubleValue1 + doubleValue2
@@ -95,15 +105,18 @@ var value2: [String] = []
             result = doubleValue1 / doubleValue2
             print(result)
         case .invertSign:
-            
-        case .none:
-            result = Double(value1.joined())
-            print(result)
+            if isPressed {
+                doubleValue2 *= -1
+            } else {
+                doubleValue1 *= -1
+            }
         }
     }
     isPressed = false
-    currentOperator = .none
 }
+
+var result: Double
+
 
 
 
@@ -118,3 +131,5 @@ print(value2)
 print(value1)
 print(value2)
 equals()
+
+
