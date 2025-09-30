58ae9759962bc8a5cd0bca8a2694c3d2b230b7af
Ezra Pease <pieezra@gmail.com>
2025-09-30 09:18:05 -0600

    Follow Along 9-30

---
+++ Other/FollowAlong 9-30/FollowAlong 9-30/FollowAlong_9_30App.swift
@@ -0,0 +1,17 @@
+//
+//  FollowAlong_9_30App.swift
+//  FollowAlong 9-30
+//
+//  Created by Ezra Pease on 9/30/25.
+//
+
+import SwiftUI
+
+@main
+struct FollowAlong_9_30App: App {
+    var body: some Scene {
+        WindowGroup {
+            ShapesView()
+        }
+    }
+}
