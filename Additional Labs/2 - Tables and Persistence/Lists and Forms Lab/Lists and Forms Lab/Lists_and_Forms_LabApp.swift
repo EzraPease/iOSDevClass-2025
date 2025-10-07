a6de55f1f60abe7d2a55f8560ccbcafbdec215cf
Ezra Pease <pieezra@gmail.com>
2025-10-07 09:04:02 -0600

    Added TP01 Lab: List and Forms

---
+++ Additional Labs/2 - Tables and Persistence/Lists and Forms Lab/Lists and Forms Lab/Lists_and_Forms_LabApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lists_and_Forms_LabApp.swift
+//  Lists and Forms Lab
+//
+//  Created by Ezra Pease on 10/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Lists_and_Forms_LabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
