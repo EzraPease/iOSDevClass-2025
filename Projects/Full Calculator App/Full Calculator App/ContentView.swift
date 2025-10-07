b5d84839d63d26258236434cbe38bbef5a91532b
Ezra Pease <pieezra@gmail.com>
2025-10-06 19:44:00 -0600

    Progress made on personal calculator project. Added number buttons and placeholder for the mathmatical output view

--- Projects/Full Calculator App/Full Calculator App/ContentView.swift
+++ Projects/Full Calculator App/Full Calculator App/ContentView.swift
@@ -7,12 +7,93 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            TextField(text: )
+        HStack {
+            VStack {
+                Text("Placeholder")
+                    .font(.largeTitle)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(.gray)
+                
+                Spacer()
+                
+                HStack {
+                    Button("1") {
+                        
+                    }
+                    .padding()
+                    .background(.gray, in: Circle())
+                    Button("2") {
+                        
+                    }
+                    .padding()
+                    .background(.gray, in: Circle())
+                    Button("3") {
+                        
+                    }
+                    .padding()
+                    .background(.gray, in: Circle())
+                    
+                }
+                HStack {
+                    Button("4") {
+                        
+                    }
+                    .padding()
+                    .background(.gray, in: Circle())
+                    Button("5") {
+                        
+                    }
+                    .padding()
+                    .background(.gray, in: Circle())
+                    Button("6") {
+                        
+                    }
+                    .padding()
+                    .background(.gray, in: Circle())
+                }
+                HStack {
+                    Button("7") {
+                        
+                    }
+                    .padding()
+                    .background(.gray, in: Circle())
+                    Button("8") {
+                        
+                    }
+                    .padding()
+                    .background(.gray, in: Circle())
+                    Button("9") {
+                        
+                    }
+                    .padding()
+                    .background(.gray, in: Circle())
+                }
+                Button("10") {
+                    
+                }
+                .padding()
+                .background(.gray, in: Circle())
+            }
+            VStack {
+                Button("+") {
+                    
+                }
+                Button("-") {
+                    
+                }
+                Button("x") {
+                    
+                }
+                Button("/") {
+                    
+                }
+            }
         }
-        .padding()
+        Spacer()
+        
     }
 }
 
