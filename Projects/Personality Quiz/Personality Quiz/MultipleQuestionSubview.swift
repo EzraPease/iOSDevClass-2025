5bb5f0936641cb11b19f7b366356972db47428fd
Ezra Pease <pieezra@gmail.com>
2025-10-11 12:12:12 -0600

    Added Iphones image for TitleView

--- Projects/Personality Quiz/Personality Quiz/MultipleQuestionSubview.swift
+++ Projects/Personality Quiz/Personality Quiz/MultipleQuestionSubview.swift
@@ -1,18 +1,14 @@
 //
-//  QuestionFlowView.swift
+//  MultipleQuestionSubview.swift
 //  Personality Quiz
 //
-//  Created by Ezra Pease on 10/10/25.
+//  Created by Ezra Pease on 10/11/25.
 //
 
 import SwiftUI
 
-struct QuestionFlowView: View {
+struct MultipleQuestionSubview: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
-}
-
-#Preview {
-    QuestionFlowView()
 }
--- Projects/Personality Quiz/Personality Quiz/TitleView.swift
+++ Projects/Personality Quiz/Personality Quiz/TitleView.swift
@@ -10,24 +10,31 @@ import SwiftUI
 struct TitleView: View {
     var body: some View {
         NavigationStack {
-            VStack {
-                Text("Which iPhone Are You?")
-                    .font(.largeTitle)
-                    .bold()
-                    .shadow(color: .gray ,radius: 6, x: 2, y: 1,)
-                    .padding()
-                
-                NavigationLink {
-                    QuestionFlowView()
-                } label: {
-                    Text("Begin Quiz")
+                VStack {
+                    Image("Iphones")
+                        .resizable()
+                        .scaledToFit()
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
                         .padding()
-                        .glassEffect()
-                        .background(.gray)
-                        .foregroundStyle(.black)
-                        .clipShape(RoundedRectangle(cornerRadius: 60))
+                        .shadow(color: .gray ,radius: 8)
+                    
+                    Text("Which iPhone Are You?")
+                        .font(.largeTitle)
+                        .bold()
+                        .shadow(color: .gray ,radius: 5, x: 2, y: 1,)
+                        .padding()
+                    
+                    NavigationLink {
+                        QuestionFlowView()
+                    } label: {
+                        Text("Begin Quiz")
+                            .padding()
+                            .glassEffect()
+                            .background(.gray)
+                            .foregroundStyle(.black)
+                            .clipShape(RoundedRectangle(cornerRadius: 60))
+                    }
                 }
-            }
         }
     }
 }
