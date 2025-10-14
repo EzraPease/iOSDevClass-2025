2dc4a676d425f85304206cd5fd751b5c20a2c54c
Ezra Pease <pieezra@gmail.com>
2025-10-14 08:15:33 -0600

    Set up the basic toggles for SingleQuestionSubview

--- Projects/Personality Quiz/Personality Quiz/QuestionFlowView.swift
+++ Projects/Personality Quiz/Personality Quiz/QuestionFlowView.swift
@@ -13,6 +13,86 @@ struct QuestionFlowView: View {
     }
 }
 
+struct SingleQuestionSubview: View {
+    @State private var questionOneIsOn = false
+    @State private var questionTwoIsOn = false
+    @State private var questionThreeIsOn = false
+    @State private var questionFourIsOn = false
+    
+    var body: some View {
+        NavigationStack {
+            Toggle(isOn: $questionOneIsOn) {
+                Text("Question 1")
+                    .font(.title3)
+                    .bold()
+            }
+                .padding()
+            Toggle(isOn: $questionTwoIsOn) {
+                Text("Question 2")
+                    .font(.title3)
+                    .bold()
+            }
+                .padding()
+            Toggle(isOn: $questionThreeIsOn) {
+                Text("Question 3")
+                    .font(.title3)
+                    .bold()
+            }
+                .padding()
+            Toggle(isOn: $questionFourIsOn) {
+                Text("Question 4")
+                    .font(.title3)
+                    .bold()
+            }
+                .padding()
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        NavigationLink {
+                            MultipleQuestionSubview()
+                        } label: {
+                            Text("Next")
+                        }
+                    }
+                }
+        }
+    }
+}
+
+struct RangedQuestionSubview: View {
+    var body: some View {
+        NavigationStack {
+            Text("Insert Question Here")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        NavigationLink {
+                            SingleQuestionSubview()
+                        } label: {
+                            Text("Next")
+                        }
+                    }
+                }
+        }
+    }
+}
+
+
+struct MultipleQuestionSubview: View {
+    var body: some View {
+        NavigationStack {
+            Text("Insert Question Here")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        NavigationLink {
+                            ResultsView()
+                        } label: {
+                            Text("Next")
+                        }
+                    }
+                }
+        }
+    }
+}
+
 #Preview {
-    QuestionFlowView()
+    SingleQuestionSubview()
 }
