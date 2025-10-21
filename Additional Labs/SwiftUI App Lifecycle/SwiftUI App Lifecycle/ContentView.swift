e030ec2e263b28ab15c9b09b0c41e9eec7dc7007
Ezra Pease <pieezra@gmail.com>
2025-10-21 11:40:59 -0600

    Completed App lifecycle lab

--- Additional Labs/SwiftUI App Lifecycle/SwiftUI App Lifecycle/ContentView.swift
+++ Additional Labs/SwiftUI App Lifecycle/SwiftUI App Lifecycle/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct Events {
-var scene: String = ""
+    var scene: String = ""
 }
 
 
@@ -16,17 +16,55 @@ var scene: String = ""
 struct ContentView: View {
     @Environment(\.scenePhase) var scenePhase
     
+    @State private var eventScene = Events()
+    
     var body: some View {
-        Text("\(Events.scene)")
-            .onChange(of: scenePhase) { _, newEvent in
-                if newEvent == .inactive {
-                    
-                } else if newEvent == .active {
-                    
-                } else if newEvent == .background {
-                    
-                }
+        NavigationStack {
+            VStack {
+                Text("App Status:")
+                    .bold()
+                    .underline()
+                    .font(.largeTitle)
+                    .padding()
+                Text("\(eventScene.scene)")
+                    .onChange(of: scenePhase) { _, newEvent in
+                        if newEvent == .inactive {
+                            eventScene.scene.append("\nApp no longer active")
+                        } else if newEvent == .active {
+                            eventScene.scene.append("\nApp now active")
+                        } else if newEvent == .background {
+                            eventScene.scene.append("\nApp backgrounded")
+                        }
+                    }
+                    .toolbar {
+                        NavigationLink("Other Status Stuff") {
+                            SecondContentView()
+                        }
+                    }
             }
+            .onAppear {
+                print("First view appeared")
+            }
+        }
+    }
+}
+
+
+struct SecondContentView: View {
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "dog.fill")
+                .resizable()
+                .scaledToFit()
+                .padding()
+                .onAppear {
+                    print("SecondContentView Appeared")
+                }
+        }
+        .onDisappear() {
+            print("The second view is no more")
+        }
     }
 }
 
