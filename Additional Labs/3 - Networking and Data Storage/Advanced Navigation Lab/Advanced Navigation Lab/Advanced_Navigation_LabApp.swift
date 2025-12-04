200471e90789e6bd1c22b1befb348ce90a09db55
Ezra Pease <pieezra@gmail.com>
2025-12-04 09:03:27 -0700

    Created Advanced Nav lab

---
+++ Additional Labs/3 - Networking and Data Storage/Advanced Navigation Lab/Advanced Navigation Lab/Advanced_Navigation_LabApp.swift
@@ -0,0 +1,17 @@
+//
+//  Advanced_Navigation_LabApp.swift
+//  Advanced Navigation Lab
+//
+//  Created by Ezra Pease on 12/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Advanced_Navigation_LabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Additional Labs/3 - Networking and Data Storage/Advanced Navigation Lab/Advanced Navigation Lab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Advanced Navigation Lab
+//
+//  Created by Ezra Pease on 12/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
