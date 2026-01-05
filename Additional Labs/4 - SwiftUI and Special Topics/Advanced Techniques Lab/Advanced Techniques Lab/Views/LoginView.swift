214c576e7f3fdc5d2a1deb5b458179bcb5bd58cc
Ezra Pease <pieezra@gmail.com>
2026-01-05 12:47:15 -0700

    Added some extra fun stuff cause why not! Including: Forgot Password Logic (kinda), Fixed the show password button image, Added a Login title

--- Additional Labs/4 - SwiftUI and Special Topics/Advanced Techniques Lab/Advanced Techniques Lab/Views/LoginView.swift
+++ Additional Labs/4 - SwiftUI and Special Topics/Advanced Techniques Lab/Advanced Techniques Lab/Views/LoginView.swift
@@ -14,9 +14,15 @@ struct LoginView: View {
     @State private var loginState: LoginViewModel.LoginState = .idle
     @State private var viewModel = LoginViewModel()
     @State private var passwordHidden = true
+    @State private var showForgotPassword = false
+    @State private var loginDisabled = false
     
     var body: some View {
         VStack(alignment: .center) {
+            Text("Login")
+                .font(.largeTitle)
+                .bold()
+            
             VStack {
                 switch loginState {
                 case .idle:
@@ -35,7 +41,7 @@ struct LoginView: View {
             .padding()
             
             VStack(alignment: .leading, spacing: 10) {
-            TextField("Username", text: $username)
+                TextField("Username", text: $username)
                     .modifier(CustomTextFeildStyle())
                 HStack {
                     if passwordHidden {
@@ -49,9 +55,9 @@ struct LoginView: View {
                         showPassword()
                     } label: {
                         if passwordHidden {
-                            Image(systemName: "eye.fill")
-                        } else {
                             Image(systemName: "eye.slash.fill")
+                        } else {
+                            Image(systemName: "eye.fill")
                         }
                     }
                 }
@@ -61,15 +67,27 @@ struct LoginView: View {
                 Task { await attemptLogin() }
             }
             .buttonStyle(CustomButtonStyle())
-                .padding(.vertical)
+            .padding(.vertical)
+            .disabled(loginDisabled)
             VStack(alignment: .trailing) {
                 Button("Forgot Password") {
-                    
+                    Task { await rememberPassword() }
                 }
                 .buttonStyle(SecondCustomButtonStyle())
             }
+            
+            Text("Congrats! You now remember your password! :D")
+                .padding(1)
+                .opacity(showForgotPassword ? 1 : 0)
         }
         .padding()
+    }
+    
+    func rememberPassword() async {
+        showForgotPassword = true
+        
+        try? await Task.sleep(for: .seconds(5))
+        showForgotPassword = false
     }
     
     func showPassword() {
@@ -77,20 +95,23 @@ struct LoginView: View {
     }
     
     func attemptLogin() async {
+        loginDisabled = true
         loginState = .loading
         
         // Loading...
         try? await Task.sleep(for: .seconds(1))
         if username.isEmpty || password.isEmpty {
             loginState = .error("Please input username and password")
+            loginDisabled = false
         } else {
             loginState = .success
             try? await Task.sleep(for: .seconds(1))
             username = ""
             password = ""
+            loginDisabled = false
         }
         
-        try? await Task.sleep(for: .seconds(5))
+        try? await Task.sleep(for: .seconds(3))
         loginState = .idle
     }
 }
