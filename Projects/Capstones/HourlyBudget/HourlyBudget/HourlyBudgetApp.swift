cfbf56a66ae7c271355df4470f1a37b7233875b9
Ezra Pease <pieezra@gmail.com>
2026-01-08 10:38:34 -0700

    Created capstone project: Budgeting App

---
+++ Projects/Capstones/HourlyBudget/HourlyBudget/HourlyBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  HourlyBudgetApp.swift
+//  HourlyBudget
+//
+//  Created by Ezra Pease on 1/8/26.
+//
+
+import SwiftUI
+
+@main
+struct HourlyBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
